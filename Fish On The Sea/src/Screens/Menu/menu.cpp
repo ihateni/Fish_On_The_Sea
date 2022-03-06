f22d854ef7bdd42e789323dce158b310dfd50261
martin <martinconcetti97@gmail.com>
2022-03-06 02:19:20 -0300

    Texture for play, controls, credits and exit buttons

--- Fish On The Sea/src/Screens/Menu/menu.cpp
+++ Fish On The Sea/src/Screens/Menu/menu.cpp
@@ -14,11 +14,41 @@ namespace fish {
 		static Rectangle rec2M;
 		static Rectangle rec3M;
 		static Rectangle rec4M;
-		Texture2D texture;
+
+		Texture2D play1;
+		Texture2D play2;
+		bool playState;
+
+		Texture2D control1;
+		Texture2D control2;
+		bool controlsState;
+
+		Texture2D credits1;
+		Texture2D credits2;
+		bool creditsState;
+
+		Texture2D exit1;
+		Texture2D exit2;
+		bool exitState;
 
 		void menuInit() {
-			texture  = LoadTexture("res/credits1.png");
-			rec1M.height = (static_cast<float> (GetScreenHeight()) - ((static_cast<float>(GetScreenHeight()) / 10) * 2)) / 10;
+			play1 = LoadTexture("res/play1.png");
+			play2 = LoadTexture("res/play2.png");
+			playState = true;
+
+			credits1 = LoadTexture("res/credits1.png");
+			credits2 = LoadTexture("res/credits2.png");
+			creditsState = true;
+
+			control1 = LoadTexture("res/instructions1.png");
+			control2 = LoadTexture("res/instructions2.png");
+			controlsState = true;
+
+			exit1 = LoadTexture("res/exit1.png");
+			exit2 = LoadTexture("res/exit2.png");
+			exitState = true;
+
+			rec1M.height = (static_cast<float> (GetScreenHeight()) - ((static_cast<float>(GetScreenHeight()) / 10) * 2)) / 15;
 			rec1M.width = (static_cast<float> (GetScreenWidth()) - ((static_cast<float>(GetScreenWidth()) / 10) * 2)) / 3;
 			rec1M.x = static_cast<float>(GetScreenWidth()) / 2 - rec1M.width / 2;
 			rec1M.y = static_cast<float>(GetScreenHeight()) / 2 - rec1M.height;
@@ -28,12 +58,12 @@ namespace fish {
 			rec2M.x = static_cast<float>(GetScreenWidth()) / 2 - rec1M.width / 2;
 			rec2M.y = static_cast<float>(GetScreenHeight()) / 2 + rec1M.height;
 
-			rec3M.height = (static_cast<float> (GetScreenHeight()) - ((static_cast<float>(GetScreenHeight()) / 10) * 2)) / 10;
+			rec3M.height = (static_cast<float> (GetScreenHeight()) - ((static_cast<float>(GetScreenHeight()) / 10) * 2)) / 15;
 			rec3M.width = (static_cast<float> (GetScreenWidth()) - ((static_cast<float>(GetScreenWidth()) / 10) * 2)) / 3;
-			rec3M.x = static_cast<float>(GetScreenWidth()) / 2 - rec1M.width  / 2;
+			rec3M.x = static_cast<float>(GetScreenWidth()) / 2 - rec1M.width / 2;
 			rec3M.y = static_cast<float>(GetScreenHeight()) / 2 + rec1M.height * 3;
 
-			rec4M.height = (static_cast<float> (GetScreenHeight()) - ((static_cast<float>(GetScreenHeight()) / 10) * 2)) / 10;
+			rec4M.height = (static_cast<float> (GetScreenHeight()) - ((static_cast<float>(GetScreenHeight()) / 10) * 2)) / 15;
 			rec4M.width = (static_cast<float> (GetScreenWidth()) - ((static_cast<float>(GetScreenWidth()) / 10) * 2)) / 3;
 			rec4M.x = static_cast<float>(GetScreenWidth()) / 2 - rec1M.width / 2;
 			rec4M.y = static_cast<float>(GetScreenHeight()) / 2 + rec1M.height * 5;
@@ -44,28 +74,73 @@ namespace fish {
 
 			mousePoint = GetMousePosition();
 
+			//play button
 			if (CheckCollisionPointRec(mousePoint, rec1M)) {
-				if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+				if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+					playState = false;
+				}
+				else {
+					playState = true;
+				}
+
+				if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
 					gameManager::Screens = gameManager::GameScreen::Game;
 				}
 			}
+			else {
+				playState = true;
 
+			}
+
+			//credits button
 			if (CheckCollisionPointRec(mousePoint, rec2M)) {
-				if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+				if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+					creditsState = false;
+				}
+				else {
+					creditsState = true;
+				}
+				if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
 					//gameManager::Screens = gameManager::GameScreen::Credits;
 				}
 			}
+			else {
+				creditsState = true;
 
+			}
+
+			//control button
 			if (CheckCollisionPointRec(mousePoint, rec3M)) {
-				if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+				if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+					controlsState = false;
+				}
+				else {
+					controlsState = true;
+				}
+				if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
 					//gameManager::Screens = gameManager::GameScreen::Controls;
 				}
 			}
+			else {
+				controlsState = true;
 
+			}
+
+			//exit button
 			if (CheckCollisionPointRec(mousePoint, rec4M)) {
-				if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+				if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+					exitState = false;
+				}
+				else {
+					exitState = true;
+				}
+				if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
 					gameManager::Screens = gameManager::GameScreen::End;
 				}
+			}
+			else {
+				exitState = true;
+
 			}
 		}
 
@@ -73,18 +148,44 @@ namespace fish {
 			BeginDrawing();
 			ClearBackground(RAYWHITE);
 
+#if _DEBUG
 			DrawRectangle(static_cast<int>(rec1M.x), static_cast<int>(rec1M.y), static_cast<int>(rec1M.width), static_cast<int>(rec1M.height), BLACK);
 			DrawRectangle(static_cast<int>(rec2M.x), static_cast<int>(rec2M.y), static_cast<int>(rec2M.width), static_cast<int>(rec2M.height), BLACK);
-			DrawTexture(texture,static_cast<int>(rec2M.x), static_cast<int>(rec2M.y) - 45, WHITE);
 			DrawRectangle(static_cast<int>(rec3M.x), static_cast<int>(rec3M.y), static_cast<int>(rec3M.width), static_cast<int>(rec3M.height), BLACK);
 			DrawRectangle(static_cast<int>(rec4M.x), static_cast<int>(rec4M.y), static_cast<int>(rec4M.width), static_cast<int>(rec4M.height), BLACK);
+#endif
 
-			DrawText("Play", static_cast<int>(rec1M.x) + 18, static_cast<int>(rec1M.y) + 10, 20, WHITE);
-			DrawText("Controls", static_cast<int>(rec3M.x) + 5, static_cast<int>(rec3M.y) + 10, 20, WHITE);
-			DrawText("Exit", static_cast<int>(rec4M.x) + 18, static_cast<int>(rec4M.y) + 10, 20, WHITE);
+			if (playState) {
+				DrawTexture(play1, static_cast<int>(rec1M.x), static_cast<int>(rec1M.y) - 45, WHITE);
+			}
+			else {
+				DrawTexture(play2, static_cast<int>(rec1M.x), static_cast<int>(rec1M.y) - 45, WHITE);
+			}
 
+			if (creditsState) {
+				DrawTexture(credits1, static_cast<int>(rec2M.x), static_cast<int>(rec2M.y) - 45, WHITE);
+			}
+			else {				
+				DrawTexture(credits2, static_cast<int>(rec2M.x), static_cast<int>(rec2M.y) - 45, WHITE);
+			}
+
+			if (controlsState) {
+				DrawTexture(control1, static_cast<int>(rec3M.x), static_cast<int>(rec3M.y) - 45, WHITE);
+			}
+			else {
+				DrawTexture(control2, static_cast<int>(rec3M.x), static_cast<int>(rec3M.y) - 45, WHITE);
+			}
+
+			if (exitState) {
+				DrawTexture(exit1, static_cast<int>(rec4M.x), static_cast<int>(rec4M.y) - 45, WHITE);
+			}
+			else {
+				DrawTexture(exit2, static_cast<int>(rec4M.x), static_cast<int>(rec4M.y) - 45, WHITE);
+			}
+
+			
 			DrawText("Fish On The Sea", static_cast<int>(GetScreenWidth() - 450), 100, 50, RED);
-		
+
 			DrawText("V0.5", static_cast<int>(GetScreenWidth() - 100), static_cast<int>(GetScreenHeight()) - 40, 20, SKYBLUE);
 
 			EndDrawing();
